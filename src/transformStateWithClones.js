ea2ae187d625f10601c1c134b8f356fe9416ad6d
Dina Zhyliaieva <din.zhyliaieva@gmail.com>
2025-06-11 15:46:36 +0300

    transformStateWithClones

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,36 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const stateCopy = { ...state };
+  const stateHistorie = [];
+
+  for (const action of actions) {
+    switch (action.type) {
+      case 'addProperties':
+        Object.assign(stateCopy, action.extraData);
+        break;
+
+      case 'removeProperties':
+        for (let i = 0; i < action.keysToRemove.length; i++) {
+          delete stateCopy[action.keysToRemove[i]];
+        }
+
+        break;
+
+      case 'clear':
+        for (const key of Object.keys(stateCopy)) {
+          if (stateCopy[key]) {
+            delete stateCopy[key];
+          }
+        }
+
+        break;
+    }
+    stateHistorie.push({ ...stateCopy });
+  }
+
+  // Return the history of states
+  return stateHistorie;
 }
 
 module.exports = transformStateWithClones;
